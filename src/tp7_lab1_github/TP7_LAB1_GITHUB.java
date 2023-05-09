88e7b871074ff3ac48a4ed40c741d34e6b38e632
PascualiDarius12 <dario12bifa@gmail.com>
2023-05-08 21:34:36 -0300

    Initial commit

---
+++ src/tp7_lab1_github/TP7_LAB1_GITHUB.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tp7_lab1_github;
+
+/**
+ *
+ * @author 54266
+ */
+public class TP7_LAB1_GITHUB {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
