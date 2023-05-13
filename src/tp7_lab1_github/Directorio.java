bc9ff5b9001faf16144e22bce3e5c993aab48a22
PascualiDarius12 <dario12bifa@gmail.com>
2023-05-13 14:25:46 -0300

    dfsd

--- src/tp7_lab1_github/Directorio.java
+++ src/tp7_lab1_github/Directorio.java
@@ -1,28 +1,24 @@
-
-package tp7;
+package tp7_lab1_github;
 
 import java.util.HashMap;
-import java.util.HashSet;
 
 /**
  *
  * @author 54266
  */
 public class Directorio {
-    
-   public  HashMap<String, Materia> materiasEscuela = new HashMap();
-   public  HashMap<String, Alumno> alumnosEscuela = new HashMap();
+
+    public HashMap<String, Materia> materiasEscuela = new HashMap();
+    public HashMap<String, Alumno> alumnosEscuela = new HashMap();
 
     public Directorio() {
     }
 
-    
-     
-     public boolean agregarMateria(Materia m1, String numero) {
+    public boolean agregarMateria(Materia m1, String numero) {
 
         boolean agregado = false;
 
-        if (!materiasEscuela.containsKey(numero)) { 
+        if (!materiasEscuela.containsKey(numero)) {
             materiasEscuela.put(numero, m1);
             agregado = true;
         }
@@ -30,12 +26,12 @@ public class Directorio {
         return agregado;
 
     }
-     
+
     public boolean agregarAlumno(Alumno a, String numero) {
 
         boolean agregado = false;
 
-        if (!alumnosEscuela.containsKey(numero)) { 
+        if (!alumnosEscuela.containsKey(numero)) {
             alumnosEscuela.put(numero, a);
             agregado = true;
         }
@@ -43,7 +39,5 @@ public class Directorio {
         return agregado;
 
     }
-    
-  
-   
+
 }
